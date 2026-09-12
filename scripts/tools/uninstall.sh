e5d7eff7c608811e265911a87c270fb9a0192394
apsopen <wipeyouripad@gmail.com>
2026-09-12 04:07:48 +0000

    gsdgsd

--- scripts/tools/uninstall.sh
+++ scripts/tools/uninstall.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cd "$HOME/Library/Printers"
-rm -rf *
-osascript -e 'Remove the app from your dock, empty you trash, and you're good!"
+(
+    cd "$HOME/Library/Printers" || exit 1
+    rm -rf *
+    osascript -e 'display dialog "Remove the app from your Dock, empty your Trash, and you'\''re good!"'
+) &
