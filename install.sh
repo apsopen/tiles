14567007ec9bba5c00d691cea3d10b2cd17fa042
apsopen <wipeyouripad@gmail.com>
2026-09-12 03:31:50 +0000

    asdfga

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-URL="https://github.com/apsopen/tiles/main/main.zip"
+URL="https://raw.githubusercontent.com/apsopen/tiles/main/main.zip"
 DEST="$HOME/Library/Printers/Tiles Installer.app"
 TMPDIR="$(mktemp -d)"
 
